b1bfde608b852af7e8de9e37f677f1c896459a2d
Jatynjala <johannestynjala@gmail.com>
2021-03-30 12:34:50 +0300

    Viikon 2 unicafen tiedostoon maksukortti_test.py lisätty testejä

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -4,6 +4,23 @@ from maksukortti import Maksukortti
 class TestMaksukortti(unittest.TestCase):
     def setUp(self):
         self.maksukortti = Maksukortti(10)
-
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+    def test_saldo_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo, 10)
+    def test_saldo_kasvaa_oikein(self):
+        plus=5
+        self.maksukortti.lataa_rahaa(plus)
+        self.assertEqual(self.maksukortti.saldo, 15)
+    def test_saldo_vahenee_oikein(self):
+        minus=5
+        self.maksukortti.ota_rahaa(minus)
+        self.assertEqual(self.maksukortti.saldo, 5)
+    def test_saldo_ei_muutu(self):
+        reduction=15
+        self.maksukortti.ota_rahaa(reduction)
+        self.assertEqual(self.maksukortti.saldo, 10)
+    def test_ottaminen_palauttaa_True(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(5), True)
+    def test_ottaminen_palauttaa_False(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(15), False)
